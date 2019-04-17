2482d4eb733b6282d3c675ae9a8d0c2b65548e50
haphi <haphi33chyne@gmail.com>
2019-04-17 15:16:38 +0530

    Implemented email and google login

--- app/src/main/java/com/softwareengineeringproject/collaborativedoodling/activity/MainActivity.kt
+++ app/src/main/java/com/softwareengineeringproject/collaborativedoodling/activity/MainActivity.kt
@@ -2,12 +2,36 @@ package com.softwareengineeringproject.collaborativedoodling.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.softwareengineeringproject.collaborativedoodling.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+//            val photoUrl = user.photoUrl
+
+            // Check if user's email is verified
+//            val emailVerified = user.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val uid = user.uid
+
+            titleTV.text = " Hi, $name !"
+
+        }
+
     }
+
+
 }
