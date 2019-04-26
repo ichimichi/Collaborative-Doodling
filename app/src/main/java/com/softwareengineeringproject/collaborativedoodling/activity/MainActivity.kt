4d0a9e5135c836eca9df2f73dc0581bb2d015167
ichimichi <ichimichilaribok@gmail.com>
2019-04-26 23:44:20 +0530

    updated MainActivity Design

--- app/src/main/java/com/softwareengineeringproject/collaborativedoodling/activity/MainActivity.kt
+++ app/src/main/java/com/softwareengineeringproject/collaborativedoodling/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
@@ -14,9 +15,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val headingList: ArrayList<LottieAnimationView> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        populateHeadingList()
 
         val user = FirebaseAuth.getInstance().currentUser
 
@@ -41,6 +46,12 @@ class MainActivity : AppCompatActivity() {
         profileBtn.setOnClickListener {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
+        }
+
+        for(heading:LottieAnimationView in headingList){
+            heading.setOnClickListener {
+                heading.resumeAnimation()
+            }
         }
 
 
@@ -100,11 +111,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun populateHeadingList() {
+        headingList.add(heading11)
+        headingList.add(heading12)
+        headingList.add(heading13)
+        headingList.add(heading14)
+        headingList.add(heading15)
+        headingList.add(heading16)
+        headingList.add(heading17)
+        headingList.add(heading18)
+        headingList.add(heading19)
+        headingList.add(heading110)
+        headingList.add(heading111)
+        headingList.add(heading112)
+        headingList.add(heading113)
+        headingList.add(heading21)
+        headingList.add(heading22)
+        headingList.add(heading23)
+        headingList.add(heading24)
+        headingList.add(heading25)
+        headingList.add(heading26)
+        headingList.add(heading27)
+        headingList.add(heading28)
+    }
 
     companion object {
 
         private const val RC_SIGN_IN = 123
     }
+
 
 
 }
