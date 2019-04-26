7f26e1fc3eb631ff80bfa6776345063d533c6494
ichimichi <ichimichilaribok@gmail.com>
2019-04-26 22:32:44 +0530

    added animations to main and session option layout

--- app/src/main/java/com/softwareengineeringproject/collaborativedoodling/activity/sessionOptionActivity.kt
+++ app/src/main/java/com/softwareengineeringproject/collaborativedoodling/activity/sessionOptionActivity.kt
@@ -8,14 +8,42 @@ import kotlinx.android.synthetic.main.activity_session_option.*
 
 class sessionOptionActivity : AppCompatActivity() {
 
+    companion object {
+        var index: Int = 0
+    }
+
+    val strokeList: ArrayList<Int> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_session_option)
+
+        populateStrokeList()
 
         joinRoomBtn.setOnClickListener {
             val intent = Intent(this, DoodlingActivity::class.java)
             intent.putExtra(DoodlingActivity.ROOM_NAME, roomNameET.text.toString())
             startActivity(intent)
         }
+
+        pencilAV.setOnClickListener {
+            index = ((index + 1) % strokeList.size )
+            pencilAV.setAnimation(strokeList[index])
+            pencilAV.resumeAnimation()
+        }
+
+
+    }
+
+    fun populateStrokeList() {
+        strokeList.add(R.raw.stroke_1)
+        strokeList.add(R.raw.stroke_2)
+        strokeList.add(R.raw.stroke_3)
+        strokeList.add(R.raw.stroke_4)
+        strokeList.add(R.raw.stroke_5)
+        strokeList.add(R.raw.stroke_6)
+        strokeList.add(R.raw.stroke_7)
+        strokeList.add(R.raw.stroke_8)
+        strokeList.add(R.raw.stroke_9)
     }
 }
